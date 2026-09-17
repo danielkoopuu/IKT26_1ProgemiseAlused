b15d42429c62d085ea6beb75ccdfff06a52ffd29
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-17 10:46:25 +0300

    IfAndElse

---
+++ 03 tund/IfAndElsenesting/IfAndElsenesting/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace IfAndElsenesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tere Daniel");
+
+            string number = Console.ReadLine();
+            //muudmae stringi andmetüüpiks ja kasutame parset
+            int numberInt = int.Parse(number);
+
+            if (numberInt == 12)
+            {
+                Console.WriteLine("number on 12 ");
+            }
+            else if (numberInt > 20)
+            {
+                if (numberInt > 30)
+                {
+                    Console.WriteLine("Sinu vanus on 31 või vanem");
+                }
+                else
+                {
+                    Console.WriteLine("Sinu vanus on 21 kuni 30 ");
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine("Mingid kahtlased väärtused");
+            }
+        }
+    }
+}
