0c1f4d5491e5f528820c0a7e2d3a5b7f8d3b166c
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-24 10:52:13 +0300

    ifelseeetodcail

---
+++ 04/ifelsemeetodcail/ifelsemeetodcail/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ifelsemeetodcail
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tere Daniel");
+            //meil on meetod mis läheb alati esimesena tööle 
+            //teha siia teine meetod ja nimeks on sellel he
+            Hellomeethod();
+            terehommikust();
+            Console.WriteLine( "kui soovid meetotit välja kutsuda siis kirjuta ja");
+            string method = Console.ReadLine();
+
+            if (method == "ja")
+            {        // kui kirjutan meetodi nime siis seda nimetatakse 
+                //meetodi välja kutsumiseks 
+                hellomethod();
+            }
+            else
+            {
+                Console.WriteLine("ei soovinud midagi");
+            }
+        }
+        static void Hellomeethod()
+        {
+            Console.WriteLine("Hello kitty");
+        }
+       
+    }
+}
